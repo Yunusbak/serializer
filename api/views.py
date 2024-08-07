0f649e4091b4e1d7e92854b4918019c504b89ec6
Yunusbak <study.yunus@gmail.com>
2024-08-07 18:53:00 +0500

    update : search

--- api/views.py
+++ api/views.py
@@ -4,16 +4,26 @@ from rest_framework.views import APIView
 from .models import Song, Album, Artist
 from .serializer import SongSerializer, AlbumSerializer, ArtistSerializer
 
+
 class ArtistView(APIView):
     def get(self, request):
-        artist = Artist.objects.all()
-        serializer = ArtistSerializer(artist, many=True)
+
+        search_query = request.GET.get('search', None)
+
+        if search_query:
+            artists = Artist.objects.filter(first_name__icontains=search_query) | Artist.objects.filter(last_name__icontains=search_query)
+        else:
+            artists = Artist.objects.all()
+
+        serializer = ArtistSerializer(artists, many=True)
         return Response(data=serializer.data)
+
     def post(self, request):
         serializer = ArtistSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ArtistDetailView(APIView):
     def get(self, request, id):
@@ -42,16 +52,22 @@ class ArtistDetailView(APIView):
 
 class AlbumView(APIView):
     def get(self, request):
-        queryset = Album.objects.all()
+        search_query = request.GET.get('search', None)
+        if search_query:
+            queryset = Album.objects.filter(title__icontains=search_query)
+        else:
+            queryset = Album.objects.all()
         serializer = AlbumSerializer(queryset, many=True)
         data = serializer.data
         return Response(data=data)
+
     def post(self, request):
         serializer = AlbumSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class AlbumDetailView(APIView):
     def get(self, request, id):
@@ -77,18 +93,24 @@ class AlbumDetailView(APIView):
         album.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 class SongView(APIView):
     def get(self, request):
-        queryset = Song.objects.all()
+        search_query = request.GET.get('search', None)
+        if search_query:
+            queryset = Song.objects.filter(title__icontains=search_query) | Song.objects.filter(artist__first_name__icontains=search_query)
+        else:
+            queryset = Song.objects.all()
+
         serializer = SongSerializer(queryset, many=True)
         return Response(data=serializer.data)
+
     def post(self, request):
         serializer = SongSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class SongDetailView(APIView):
     def get(self, request, id):
         song = Song.objects.get(id=id)
